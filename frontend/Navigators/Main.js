73bc09d263fba3576b00b4bb176531d109221033
Neel Ismail <neelismail01@gmail.com>
2021-03-07 14:30:13 -0500

    Updating bottom nav icons

---
+++ frontend/Navigators/Main.js
@@ -0,0 +1,88 @@
+import React, { useContext } from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { View } from "react-native";
+import { Icon } from 'react-native-elements'
+
+// Stacks
+import HomeNavigator from "./HomeNavigator";
+import CartNavigator from "./CartNavigator";
+import LearnNavigator from "./LearnNavigator";
+import UserNavigator from "./UserNavigator";
+import AdminNavigator from "./AdminNavigator";
+
+import CartIcon from "../Shared/CartIcon";
+import AuthGlobal from "../Context/store/AuthGlobal";
+
+const Tab = createBottomTabNavigator();
+
+const Main = () => {
+
+  const context = useContext(AuthGlobal)
+
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      tabBarOptions={{
+        keyboardHidesTabBar: true,
+        showLabel: false,
+        activeTintColor: "#e91e63",
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeNavigator}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Icon name="home" type="font-awesome-5" color={color} size={30} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Cart"
+        component={CartNavigator}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <View>
+              <Icon name="receipt" type="font-awesome-5" color={color} size={30} />
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Learn"
+        component={LearnNavigator}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <View>
+              <Icon name="book-open" type="font-awesome-5" color={color} size={30} />
+            </View>
+          ),
+        }}
+      />
+      
+      {context.stateUser.user.isAdmin == true ? (
+        <Tab.Screen
+        name="Admin"
+        component={AdminNavigator}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Icon name="cog" color={color} size={30} />
+          ),
+        }}
+      />
+      ): null }
+      
+      <Tab.Screen
+        name="User"
+        component={UserNavigator}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Icon name="user-circle" type="font-awesome-5" color={color} size={30} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+export default Main;
