91f71bb6381f7260dfda5543d0676db7bde15231
nikhil-ismail <nikhil.ismail@yale.edu>
2021-03-08 13:00:15 -0500

    neel changes

--- front-end/App.js
+++ front-end/App.js
@@ -9,7 +9,7 @@ import { Provider } from "react-redux";
 import store from "./Redux/store";
 
 // Context API
-import Auth from "./Context/store/Auth";
+//import Auth from "./Context/store/Auth";
 
 // Navigatiors
 import Main from "./Navigators/Main";
